b58318e8da2b9beb4d04fa9f77642c87c308262c
daniel <carlos@mirar.solutions>
2019-08-01 17:43:14 -0300

    enviando script 4

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ function fale(mensagem) {
     // https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis/speak
     // https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognitionResult
     // https://developer.mozilla.org/en-US/docs/Archive/B2G_OS/Firefox_OS_apps/Building_apps_for_Firefox_OS/Manifest
-    window.speechSynthesis.speak(new SpeechSynthesisUtterance(mensagem));
+    
+    // window.speechSynthesis.speak(new SpeechSynthesisUtterance(mensagem));
 };
 
 function teste_01() {
-    fale('Tentando ');
+    // fale('Tentando ');
     console.log('teste');
 }
