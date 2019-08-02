a1e83417f22222d71082427eda10eb2e4061278f
JonasVMagalhaes <jonasevmagalhaes@gmail.com>
2019-08-02 17:18:19 -0300

    "Fala e escuta funcionando"

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function fale(mensagem) {
     // https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognitionResult
     // https://developer.mozilla.org/en-US/docs/Archive/B2G_OS/Firefox_OS_apps/Building_apps_for_Firefox_OS/Manifest
 
-    // window.speechSynthesis.speak(new SpeechSynthesisUtterance(mensagem));
+    window.speechSynthesis.speak(new SpeechSynthesisUtterance(mensagem));
 };
 
 function teste_01() {
@@ -16,7 +16,7 @@ function teste_01() {
         return;
     } else {
         $target.innerText = 'API Bluetooth - COM Suporte.';
-        return; 
+        return;
     }
     // Se não tem suporte jamais chegará neste ponto defido ao "return"
     // navigator.bluetooth.requestDevice();
@@ -42,3 +42,66 @@ function forceResetApp() {
 
 }
 
+// Acessar
+
+
+// Testes reconhecer voz
+
+
+
+function escutar(){
+    try {
+        recognizer.start();
+    } catch (ex) {
+        alert("error: " + ex.message);
+    }
+}
+
+// Test browser support
+window.SpeechRecognition = window.SpeechRecognition ||
+    window.webkitSpeechRecognition ||
+    null;
+
+//caso não suporte esta API DE VOZ                              
+if (window.SpeechRecognition === null) {
+    document.getElementById('unsupported').classList.remove('hidden');
+} else {
+    var recognizer = new window.SpeechRecognition();
+    // var $nomeFalado = document.getElementById('nomeFalado');
+    // Para o reconhecedor de voz, não parar de ouvir, mesmo que tenha pausas no usuario
+    // recognizer.continuous = true
+    recognizer.onresult = function (event) {
+        // transcription.innerHTML = "";
+        for (var i = event.resultIndex; i < event.results.length; i++) {
+            if (event.results[i].isFinal) {
+                
+                var $nome = event.results[i][0].transcript;
+                acessar($nome);
+                //$nomeFalado.innerText = $nome;
+                // console.log($nome);
+                // document.getElementById('nomeFalado').innerHTML = $nome;
+                // transcription.innerHTML = event.results[i][0].transcript + ' (Taxa de acerto [0/1] : ' + event.results[i][0].confidence + ')';
+                // transcription.textContent = event.results[i][0].transcript + ' (Taxa de acerto [0/1] : ' + event.results[i][0].confidence + ')';
+            } else {
+                // transcription.innerHTML += event.results[i][0].transcript;
+            }
+        }
+    }
+    // document.querySelector("#rect").addEventListener("click", function () {
+    //     try {
+    //         recognizer.start();
+    //     } catch (ex) {
+    //         alert("error: " + ex.message);
+    //     }
+    // });
+}
+
+function acessar(nome) {
+    if(nome === 'Bruno') {
+        document.getElementById('nomeFalado').innerHTML = nome; 
+        document.getElementById('acoes').classList.remove('hidden');
+    } else {
+        fale('Desculpa, você não é quem eu espero!');
+        alert('Desculpa, você não é quem eu espero!');
+    }
+}
