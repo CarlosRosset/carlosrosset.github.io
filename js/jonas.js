57da6ed04c249ad0ef8da5f14b6efb74dad3147d
JonasVMagalhaes <jonasevmagalhaes@gmail.com>
2019-08-02 11:39:58 -0300

    ""

--- js/jonas.js
+++ js/jonas.js
@@ -7,13 +7,13 @@ function bluetooth() {
        
     navigator.bluetooth.requestDevice(options)
         .then(device => {
-            retorno = 'device ', device;
-            retorno += "Name: " + device.name + "\n";
-            retorno += "Id: " + device.id + "\n";
-            retorno += "Connected: " + device.gatt.connected + "\n";
+            retorno = " device ", device;
+            retorno += " Name: " + device.name + "\n";
+            retorno += " Id: " + device.id + "\n";
+            retorno += " Connected: " + device.gatt.connected + "\n";
         })
         .catch(error => {
-            retorno += "Argh! " + error + "\n";
+            retorno += " Argh! " + error + "\n";
         });
 
     console.log('fim');
